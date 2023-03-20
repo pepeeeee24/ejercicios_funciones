e727ea86513970e60e03a6b099e470b272f77fef
pepeeeee24 <115158334+pepeeeee24@users.noreply.github.com>
2023-03-20 14:45:38 +0100

    Add files via upload

---
+++ Ejercicios_Funciones/maximo.js
@@ -0,0 +1,11 @@
+function maximo(a,b){
+if(a>b){
+    return a;
+
+} else{
+    return b;
+}
+
+}
+let max_num = maximo(6,2)
+console.log("el max entre 6 y 2 es"+ max_num);
---
+++ Ejercicios_Funciones/saludo.js
@@ -0,0 +1,5 @@
+function saludo(nombre) {
+return "bienvenido" + nombre + " que tengas un gran dia "
+}
+let resultado_saludo = saludo(" pepe ")
+console.log(resultado_saludo);
